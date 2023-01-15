496a2e47194c0740a952b41f6a294d91f6839462
TPEcool <75689188+TPEcool@users.noreply.github.com>
2023-01-15 21:24:47 +0300

    Finally added jumo ability

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "R;Z=~snpldlQErg}_thd":
+            case "character":return img`
+. . . a a a a a a a a a a . . . 
+. . a c c c c c c c c c c a . . 
+. a c c c c c c c c c c c c a . 
+a c c c c 3 3 3 3 3 3 c c c c a 
+a c c c 3 d d d d d d 3 c c c a 
+a c c 3 d d d d d d d d 3 c c a 
+a c c 3 d d d d d d d d 3 c c a 
+a c c 3 d d d d d d d d 3 c c a 
+a c c 3 d d d d d d d d 3 c c a 
+a c c 3 d d d d d d d d 3 c c a 
+a c c 3 d d d d d d d d 3 c c a 
+a c c c 3 d d d d d d 3 c c c a 
+a c c c c 3 3 3 3 3 3 c c c c a 
+. a c c c c c c c c c c c c a . 
+. . a c c c c c c c c c c a . . 
+. . . a a a a a a a a a a . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-game.onUpdate(function () {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -50
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
 	
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(15)
+tiles.setCurrentTilemap(tilemap`level0`)
+mySprite = sprites.create(assets.image`character`, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 13))
+scene.cameraFollowSprite(mySprite)
+mySprite.setFlag(SpriteFlag.ShowPhysics, true)
+mySprite.fx = 15
+mySprite.fy = 15
+mySprite.ax = 20
+mySprite.ay = 75
+game.onUpdate(function () {
+    if (controller.right.isPressed()) {
+        mySprite.vx = 100
+    } else if (controller.left.isPressed()) {
+        mySprite.vx = -100
+    } else {
+        mySprite.vx = 0
+    }
 })
